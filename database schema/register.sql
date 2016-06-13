0315ac84d79d89dc8f2b61f9feabee148392f62b
b2simms <brent.simmons@unb.ca>
2016-06-12 22:48:16 -0300

    added database schema

---
+++ database schema/register.sql
@@ -0,0 +1,376 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 13, 2016 at 03:47 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `register`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `episodes`
+--
+
+CREATE TABLE IF NOT EXISTS `episodes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `season` int(3) DEFAULT NULL,
+  `episode` int(3) DEFAULT NULL,
+  `date` varchar(10) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `status` int(3) DEFAULT '0',
+  `assigned_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=128 ;
+
+--
+-- Dumping data for table `episodes`
+--
+
+INSERT INTO `episodes` (`id`, `season`, `episode`, `date`, `name`, `status`, `assigned_name`) VALUES
+(1, 1, 1, NULL, 'Rose', 2, ''),
+(2, 1, 2, NULL, 'The End of the World', 2, ''),
+(3, 1, 3, NULL, 'The Unquiet Dead', 2, ''),
+(4, 1, 4, NULL, 'Aliens of London World War Three', 2, ''),
+(5, 1, 5, NULL, 'Dalek', 2, ''),
+(6, 1, 6, NULL, 'The Long Game', 2, ''),
+(7, 1, 7, NULL, 'Father''s Day', 2, ''),
+(8, 1, 8, NULL, 'The Empty Child 1/2', 2, ''),
+(9, 1, 9, NULL, 'The Doctor Dances 2/2', 2, ''),
+(10, 1, 10, NULL, 'Boom Town', 2, ''),
+(11, 1, 11, NULL, 'Bad Wolf 1/2', 2, ''),
+(12, 1, 12, NULL, 'The Parting of Ways 2/2', 2, ''),
+(13, 2, 1, NULL, 'The Christmas Invasion', 2, ''),
+(14, 2, 2, NULL, 'New Earth', 2, ''),
+(15, 2, 3, NULL, 'Tooth and Claw', 2, ''),
+(16, 2, 4, NULL, 'School Reunion', 2, ''),
+(17, 2, 5, NULL, 'The Girl in the Fireplace', 2, ''),
+(18, 2, 6, NULL, 'Rise of the Cyberman 1/2', 2, ''),
+(19, 2, 7, NULL, 'The Age of Steel 2/2', 2, ''),
+(20, 2, 8, NULL, 'The Idiot''s Lantern', 2, ''),
+(21, 2, 9, NULL, 'The Impossible Planet 1/2', 2, ''),
+(22, 2, 10, NULL, 'The Satan Pit 2/2', 2, ''),
+(23, 2, 11, NULL, 'Love & Monsters', 2, ''),
+(24, 2, 12, NULL, 'Fear Hear', 2, ''),
+(25, 2, 13, NULL, 'Army of Ghosts 1/2', 2, ''),
+(26, 2, 14, NULL, 'Doomsday 2/2', 2, ''),
+(27, 3, 1, NULL, 'The Runaway Bride', 2, ''),
+(28, 3, 2, NULL, 'Smith and Jones', 2, ''),
+(29, 3, 3, NULL, 'The Shakespeare Code', 2, ''),
+(30, 3, 4, NULL, 'Gridlock', 2, ''),
+(31, 3, 5, NULL, 'Daleks in Manhattan 1/2', 2, ''),
+(32, 3, 6, NULL, 'Evolution of the Daleks 2/2', 2, ''),
+(33, 3, 7, NULL, 'The Lazarus Experiment', 2, ''),
+(34, 3, 8, NULL, '42', 2, ''),
+(35, 3, 9, NULL, 'Human Nature 1/2', 2, ''),
+(36, 3, 10, NULL, 'The Family of Blood 2/2', 2, ''),
+(37, 3, 11, NULL, 'Blink', 2, ''),
+(38, 3, 12, NULL, 'Utopia 1/3', 2, ''),
+(39, 3, 13, NULL, 'The Sound of Drums 2/3', 2, ''),
+(40, 3, 14, NULL, 'Last of the Time Lords 3/3', 2, ''),
+(41, 4, 1, NULL, 'Voyage of the Damned', 2, ''),
+(42, 4, 2, NULL, 'Partners in Crime', 2, ''),
+(43, 4, 3, NULL, 'The Fires of Pompeii', 2, ''),
+(44, 4, 4, NULL, 'Planet of the Ood', 2, ''),
+(45, 4, 5, NULL, 'The Sontaran Strategem 1/2', 2, ''),
+(46, 4, 6, NULL, 'The Poison Sky 2/2', 2, ''),
+(47, 4, 7, NULL, 'The Doctor''s Daughter', 2, ''),
+(48, 4, 8, NULL, 'The Unicorn and the Wasp', 0, ''),
+(49, 4, 9, NULL, 'Silence in the Library 1/2', 0, ''),
+(50, 4, 10, NULL, 'Forest of the Dead 2/2', 0, ''),
+(51, 4, 11, NULL, 'Midnight', 0, ''),
+(52, 4, 12, NULL, 'Turn Left', 0, ''),
+(53, 4, 13, NULL, 'The Stolen Earth 1/2', 0, ''),
+(54, 4, 14, NULL, 'Journey''s End 2/2', 0, ''),
+(55, 5, 1, NULL, 'The Next Doctor', 0, ''),
+(56, 5, 2, NULL, 'Planet of the Dead', 0, ''),
+(57, 5, 3, NULL, 'The Waters of Mars', 0, ''),
+(58, 5, 4, NULL, 'The End of Time', 0, ''),
+(59, 5, 5, NULL, 'The Eleventh Hour', 0, ''),
+(60, 5, 6, NULL, 'The Beast Below', 0, ''),
+(61, 5, 7, NULL, 'Victory of the Daleks', 0, ''),
+(62, 5, 8, NULL, 'The Time of Angels 1/2', 0, ''),
+(63, 5, 9, NULL, 'Flesh and Stone 2/2', 0, ''),
+(64, 5, 10, NULL, 'The Vampires of Venice', 0, ''),
+(65, 5, 11, NULL, 'Amy''s Choice', 0, ''),
+(66, 5, 12, NULL, 'The Hungry Earth 1/2', 0, ''),
+(67, 5, 13, NULL, 'Cold Blood 2/2', 0, ''),
+(68, 5, 14, NULL, 'Vincent and the Doctor', 1, 'Emilynn'),
+(69, 5, 15, NULL, 'The Lodger', 1, 'Emilynn'),
+(70, 5, 16, NULL, 'The Pandorica Opens 1/2', 0, ''),
+(71, 5, 17, NULL, 'The Big Bang 2/2', 0, ''),
+(72, 6, 1, NULL, 'A Christmas Carol', 1, 'Emilynn'),
+(73, 6, 2, NULL, 'The Impossible Astronaut 1/2', 0, ''),
+(74, 6, 3, NULL, 'Day of the Moon 2/2', 0, ''),
+(75, 6, 4, NULL, 'The Curse of the Black Spot', 0, ''),
+(76, 6, 5, NULL, 'The Doctor''s Wife', 0, ''),
+(77, 6, 6, NULL, 'The Rebel Flesh 1/2', 0, ''),
+(78, 6, 7, NULL, 'The Almost People 2/2', 0, ''),
+(79, 6, 8, NULL, 'A Good Man Goes to War', 0, ''),
+(80, 6, 9, NULL, 'Let''s Kill Hitler', 1, 'Emilynn'),
+(81, 6, 10, NULL, 'Night Terrors', 0, ''),
+(82, 6, 11, NULL, 'The Girl Who Waited', 0, ''),
+(83, 6, 12, NULL, 'The God Complex', 0, ''),
+(84, 6, 13, NULL, 'Closing Time', 0, ''),
+(85, 6, 14, NULL, 'The Wedding of River Song', 0, ''),
+(86, 7, 1, NULL, 'The Doctor, the Widow, and the Wardrobe', 0, ''),
+(87, 7, 2, NULL, 'Asylum of the Daleks', 0, ''),
+(88, 7, 3, NULL, 'Dinosaurs on a Spaceship', 0, ''),
+(89, 7, 4, NULL, 'A Town Called Mercy', 0, ''),
+(90, 7, 5, NULL, 'The Power of Three', 0, ''),
+(91, 7, 6, NULL, 'The Angels Take Manhattan', 0, ''),
+(92, 7, 7, NULL, 'The Snowmen', 0, ''),
+(93, 7, 8, NULL, 'The Bells of Saint John', 0, ''),
+(94, 7, 9, NULL, 'The Rings of Akhaten', 0, ''),
+(95, 7, 10, NULL, 'Cold War', 0, ''),
+(96, 7, 11, NULL, 'Hide', 0, ''),
+(97, 7, 12, NULL, 'Journey of the Centre of the TARDIS', 0, ''),
+(98, 7, 13, NULL, 'The Crimson Horror', 0, ''),
+(99, 7, 14, NULL, 'Nightmare in Silver', 0, ''),
+(100, 7, 15, NULL, 'The Name of the Doctor', 0, ''),
+(101, 8, 1, NULL, 'The Day of the Doctor', 0, ''),
+(102, 8, 2, NULL, 'The Time of the Doctor', 0, ''),
+(103, 8, 3, NULL, 'Deep Breath', 0, ''),
+(104, 8, 4, NULL, 'Into the Dalek', 0, ''),
+(105, 8, 5, NULL, 'Robot of Sherwood', 0, ''),
+(106, 8, 6, NULL, 'Listen', 0, ''),
+(107, 8, 7, NULL, 'Time Heist', 0, ''),
+(108, 8, 8, NULL, 'The Caretaker', 0, ''),
+(109, 8, 9, NULL, 'Kill the Moon', 0, ''),
+(110, 8, 10, NULL, 'Mummy on the Orient Express', 0, ''),
+(111, 8, 11, NULL, 'Flatline', 0, ''),
+(112, 8, 12, NULL, 'In the Forest of the Night', 0, ''),
+(113, 8, 13, NULL, 'Dark Water 1/2', 0, ''),
+(114, 8, 14, NULL, 'Death in Heaven 2/2', 0, ''),
+(115, 9, 1, NULL, 'Last Christmas', 2, ''),
+(116, 9, 2, NULL, 'The Magician''s Apprentice 1/2', 2, ''),
+(117, 9, 3, NULL, 'The Witch''s Familiar 2/2', 2, ''),
+(118, 9, 4, NULL, 'Under the Lake 1/2', 2, ''),
+(119, 9, 5, NULL, 'Before the Flood 2/2', 2, ''),
+(120, 9, 6, NULL, 'The Girl Who Died 1/2', 2, ''),
+(121, 9, 7, NULL, 'The Woman Who Lived 2/2', 2, ''),
+(122, 9, 8, NULL, 'The Zygon Invasion 1/2', 2, ''),
+(123, 9, 9, NULL, 'The Zygon Inversion 2/2', 2, ''),
+(124, 9, 10, NULL, 'Sleep No More', 2, ''),
+(125, 9, 11, NULL, 'Face the Raven 1/3', 2, ''),
+(126, 9, 12, NULL, 'Heaven Sent 2/3', 2, ''),
+(127, 9, 13, NULL, 'Hell Bent 3/3', 2, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE IF NOT EXISTS `feedback` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `sender_name` varchar(30) DEFAULT NULL,
+  `sender_email` varchar(100) DEFAULT NULL,
+  `message` text,
+  `send_date` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `sender_name`, `sender_email`, `message`, `send_date`) VALUES
+(1, 'demo2', 'demo2@yahoo.com', 'hello world\r\n', '2016-05-23 13:53:57'),
+(7, 'b2simms', 'brent.simmons@unb.ca', 'Hello Brent', '2016-05-23 14:16:37'),
+(8, '4583', 'b563@gmail.com', 'dfa', '2016-05-23 14:17:19'),
+(10, 'gfad', '5635@343.ca', 'd', '2016-05-23 14:19:46');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sheet1`
+--
+
+CREATE TABLE IF NOT EXISTS `sheet1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `season` int(3) DEFAULT NULL,
+  `episode` int(3) DEFAULT NULL,
+  `date` varchar(10) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `status` int(3) DEFAULT '0',
+  `assigned_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=128 ;
+
+--
+-- Dumping data for table `sheet1`
+--
+
+INSERT INTO `sheet1` (`id`, `season`, `episode`, `date`, `name`, `status`, `assigned_name`) VALUES
+(1, 1, 1, NULL, 'Rose', 2, ''),
+(2, 1, 2, NULL, 'The End of the World', 2, ''),
+(3, 1, 3, NULL, 'The Unquiet Dead', 2, ''),
+(4, 1, 4, NULL, 'Aliens of London World War Three', 2, ''),
+(5, 1, 5, NULL, 'Dalek', 2, ''),
+(6, 1, 6, NULL, 'The Long Game', 2, ''),
+(7, 1, 7, NULL, 'Father''s Day', 2, ''),
+(8, 1, 8, NULL, 'The Empty Child 1/2', 2, ''),
+(9, 1, 9, NULL, 'The Doctor Dances 2/2', 2, ''),
+(10, 1, 10, NULL, 'Boom Town', 2, ''),
+(11, 1, 11, NULL, 'Bad Wolf 1/2', 2, ''),
+(12, 1, 12, NULL, 'The Parting of Ways 2/2', 2, ''),
+(13, 2, 1, NULL, 'The Christmas Invasion', 2, ''),
+(14, 2, 2, NULL, 'New Earth', 2, ''),
+(15, 2, 3, NULL, 'Tooth and Claw', 2, ''),
+(16, 2, 4, NULL, 'School Reunion', 2, ''),
+(17, 2, 5, NULL, 'The Girl in the Fireplace', 2, ''),
+(18, 2, 6, NULL, 'Rise of the Cyberman 1/2', 2, ''),
+(19, 2, 7, NULL, 'The Age of Steel 2/2', 2, ''),
+(20, 2, 8, NULL, 'The Idiot''s Lantern', 2, ''),
+(21, 2, 9, NULL, 'The Impossible Planet 1/2', 2, ''),
+(22, 2, 10, NULL, 'The Satan Pit 2/2', 2, ''),
+(23, 2, 11, NULL, 'Love & Monsters', 2, ''),
+(24, 2, 12, NULL, 'Fear Hear', 2, ''),
+(25, 2, 13, NULL, 'Army of Ghosts 1/2', 2, ''),
+(26, 2, 14, NULL, 'Doomsday 2/2', 2, ''),
+(27, 3, 1, NULL, 'The Runaway Bride', 2, ''),
+(28, 3, 2, NULL, 'Smith and Jones', 2, ''),
+(29, 3, 3, NULL, 'The Shakespeare Code', 2, ''),
+(30, 3, 4, NULL, 'Gridlock', 2, ''),
+(31, 3, 5, NULL, 'Daleks in Manhattan 1/2', 2, ''),
+(32, 3, 6, NULL, 'Evolution of the Daleks 2/2', 2, ''),
+(33, 3, 7, NULL, 'The Lazarus Experiment', 2, ''),
+(34, 3, 8, NULL, '42', 2, ''),
+(35, 3, 9, NULL, 'Human Nature 1/2', 2, ''),
+(36, 3, 10, NULL, 'The Family of Blood 2/2', 2, ''),
+(37, 3, 11, NULL, 'Blink', 2, ''),
+(38, 3, 12, NULL, 'Utopia 1/3', 2, ''),
+(39, 3, 13, NULL, 'The Sound of Drums 2/3', 2, ''),
+(40, 3, 14, NULL, 'Last of the Time Lords 3/3', 2, ''),
+(41, 4, 1, NULL, 'Voyage of the Damned', 2, ''),
+(42, 4, 2, NULL, 'Partners in Crime', 2, ''),
+(43, 4, 3, NULL, 'The Fires of Pompeii', 2, ''),
+(44, 4, 4, NULL, 'Planet of the Ood', 2, ''),
+(45, 4, 5, NULL, 'The Sontaran Strategem 1/2', 2, ''),
+(46, 4, 6, NULL, 'The Poison Sky 2/2', 2, ''),
+(47, 4, 7, NULL, 'The Doctor''s Daughter', 2, ''),
+(48, 4, 8, NULL, 'The Unicorn and the Wasp', 0, ''),
+(49, 4, 9, NULL, 'Silence in the Library 1/2', 0, ''),
+(50, 4, 10, NULL, 'Forest of the Dead 2/2', 0, ''),
+(51, 4, 11, NULL, 'Midnight', 0, ''),
+(52, 4, 12, NULL, 'Turn Left', 0, ''),
+(53, 4, 13, NULL, 'The Stolen Earth 1/2', 0, ''),
+(54, 4, 14, NULL, 'Journey''s End 2/2', 0, ''),
+(55, 5, 1, NULL, 'The Next Doctor', 0, ''),
+(56, 5, 2, NULL, 'Planet of the Dead', 0, ''),
+(57, 5, 3, NULL, 'The Waters of Mars', 0, ''),
+(58, 5, 4, NULL, 'The End of Time', 0, ''),
+(59, 5, 5, NULL, 'The Eleventh Hour', 0, ''),
+(60, 5, 6, NULL, 'The Beast Below', 0, ''),
+(61, 5, 7, NULL, 'Victory of the Daleks', 0, ''),
+(62, 5, 8, NULL, 'The Time of Angels 1/2', 0, ''),
+(63, 5, 9, NULL, 'Flesh and Stone 2/2', 0, ''),
+(64, 5, 10, NULL, 'The Vampires of Venice', 0, ''),
+(65, 5, 11, NULL, 'Amy''s Choice', 0, ''),
+(66, 5, 12, NULL, 'The Hungry Earth 1/2', 0, ''),
+(67, 5, 13, NULL, 'Cold Blood 2/2', 0, ''),
+(68, 5, 14, NULL, 'Vincent and the Doctor', 1, 'Emilynn'),
+(69, 5, 15, NULL, 'The Lodger', 1, 'Emilynn'),
+(70, 5, 16, NULL, 'The Pandorica Opens 1/2', 0, ''),
+(71, 5, 17, NULL, 'The Big Bang 2/2', 0, ''),
+(72, 6, 1, NULL, 'A Christmas Carol', 1, 'Emilynn'),
+(73, 6, 2, NULL, 'The Impossible Astronaut 1/2', 0, ''),
+(74, 6, 3, NULL, 'Day of the Moon 2/2', 0, ''),
+(75, 6, 4, NULL, 'The Curse of the Black Spot', 0, ''),
+(76, 6, 5, NULL, 'The Doctor''s Wife', 0, ''),
+(77, 6, 6, NULL, 'The Rebel Flesh 1/2', 0, ''),
+(78, 6, 7, NULL, 'The Almost People 2/2', 0, ''),
+(79, 6, 8, NULL, 'A Good Man Goes to War', 0, ''),
+(80, 6, 9, NULL, 'Let''s Kill Hitler', 1, 'Emilynn'),
+(81, 6, 10, NULL, 'Night Terrors', 0, ''),
+(82, 6, 11, NULL, 'The Girl Who Waited', 0, ''),
+(83, 6, 12, NULL, 'The God Complex', 0, ''),
+(84, 6, 13, NULL, 'Closing Time', 0, ''),
+(85, 6, 14, NULL, 'The Wedding of River Song', 0, ''),
+(86, 7, 1, NULL, 'The Doctor, the Widow, and the Wardrobe', 0, ''),
+(87, 7, 2, NULL, 'Asylum of the Daleks', 0, ''),
+(88, 7, 3, NULL, 'Dinosaurs on a Spaceship', 0, ''),
+(89, 7, 4, NULL, 'A Town Called Mercy', 0, ''),
+(90, 7, 5, NULL, 'The Power of Three', 0, ''),
+(91, 7, 6, NULL, 'The Angels Take Manhattan', 0, ''),
+(92, 7, 7, NULL, 'The Snowmen', 0, ''),
+(93, 7, 8, NULL, 'The Bells of Saint John', 0, ''),
+(94, 7, 9, NULL, 'The Rings of Akhaten', 0, ''),
+(95, 7, 10, NULL, 'Cold War', 0, ''),
+(96, 7, 11, NULL, 'Hide', 0, ''),
+(97, 7, 12, NULL, 'Journey of the Centre of the TARDIS', 0, ''),
+(98, 7, 13, NULL, 'The Crimson Horror', 0, ''),
+(99, 7, 14, NULL, 'Nightmare in Silver', 0, ''),
+(100, 7, 15, NULL, 'The Name of the Doctor', 0, ''),
+(101, 8, 1, NULL, 'The Day of the Doctor', 0, ''),
+(102, 8, 2, NULL, 'The Time of the Doctor', 0, ''),
+(103, 8, 3, NULL, 'Deep Breath', 0, ''),
+(104, 8, 4, NULL, 'Into the Dalek', 0, ''),
+(105, 8, 5, NULL, 'Robot of Sherwood', 0, ''),
+(106, 8, 6, NULL, 'Listen', 0, ''),
+(107, 8, 7, NULL, 'Time Heist', 0, ''),
+(108, 8, 8, NULL, 'The Caretaker', 0, ''),
+(109, 8, 9, NULL, 'Kill the Moon', 0, ''),
+(110, 8, 10, NULL, 'Mummy on the Orient Express', 0, ''),
+(111, 8, 11, NULL, 'Flatline', 0, ''),
+(112, 8, 12, NULL, 'In the Forest of the Night', 0, ''),
+(113, 8, 13, NULL, 'Dark Water 1/2', 0, ''),
+(114, 8, 14, NULL, 'Death in Heaven 2/2', 0, ''),
+(115, 9, 1, NULL, 'Last Christmas', 2, ''),
+(116, 9, 2, NULL, 'The Magician''s Apprentice 1/2', 2, ''),
+(117, 9, 3, NULL, 'The Witch''s Familiar 2/2', 2, ''),
+(118, 9, 4, NULL, 'Under the Lake 1/2', 2, ''),
+(119, 9, 5, NULL, 'Before the Flood 2/2', 2, ''),
+(120, 9, 6, NULL, 'The Girl Who Died 1/2', 2, ''),
+(121, 9, 7, NULL, 'The Woman Who Lived 2/2', 2, ''),
+(122, 9, 8, NULL, 'The Zygon Invasion 1/2', 2, ''),
+(123, 9, 9, NULL, 'The Zygon Inversion 2/2', 2, ''),
+(124, 9, 10, NULL, 'Sleep No More', 2, ''),
+(125, 9, 11, NULL, 'Face the Raven 1/3', 2, ''),
+(126, 9, 12, NULL, 'Heaven Sent 2/3', 2, ''),
+(127, 9, 13, NULL, 'Hell Bent 3/3', 2, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `join_date` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`,`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `join_date`) VALUES
+(6, 'demo2', '$2y$10$yFZnjPdF9nrTGd6MKKLma.V6NIAPDsYeCeIftlNeUEPZLFDu4H9NC', 'demo@example.com', '2016-05-29 16:11:17'),
+(7, 'bbbbbb', '$2y$10$um4utOZV0.VaBiBjtEhS1ODkBdg5Q0HgSNd3RYTVYqDb/l/z6.d.a', 'bsim@gmail.com', '2016-05-31 11:33:47');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
